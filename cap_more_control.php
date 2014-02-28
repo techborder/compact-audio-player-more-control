796f0edfcbe295922dd08e8cfc6a6c558aa00683
David Regal <dvd.regal@gmail.com>
2014-02-27 22:14:28 -0500

    Add new shortcut to abstract plugin config. Update icons.

--- cap_more_control.php
+++ cap_more_control.php
@@ -18,9 +18,9 @@ function wp_capmc_init()
 		wp_register_script('scap.soundmanager2', C_AUDIO_MORE_CTL_BASE_URL .'js/soundmanager2-nodebug-jsmin.js');
 		wp_enqueue_script('scap.soundmanager2');
 		wp_register_style('scap.flashblock', C_AUDIO_MORE_CTL_BASE_URL .'css/flashblock.css');
-        wp_enqueue_style('scap.flashblock');
+		wp_enqueue_style('scap.flashblock');
 		wp_register_style('scap.player', C_AUDIO_MORE_CTL_BASE_URL .'css/player.css');
-        wp_enqueue_style('scap.player');
+		wp_enqueue_style('scap.player');
 	}
 }
 
@@ -29,22 +29,22 @@ function capmc_footer_code(){
 	echo "\n${debug_marker}\n";
 	?>
 	<script type="text/javascript">
-    soundManager.useFlashBlock = true; // optional - if used, required flashblock.css
-    soundManager.url = '<?php echo C_AUDIO_MORE_CTL_BASE_URL ; ?>swf/soundmanager2.swf';
-    function play_mp3(flg,ids,mp3url,volume,loops)
-    {
-      soundManager.createSound({
-        id:'btnplay_'+ids,
-        volume: volume,
-        url: mp3url
-      });
+	soundManager.useFlashBlock = true; // optional - if used, required flashblock.css
+	soundManager.url = '<?php echo C_AUDIO_MORE_CTL_BASE_URL ; ?>swf/soundmanager2.swf';
+	function play_mp3(flg,ids,mp3url,volume,loops)
+	{
+	  soundManager.createSound({
+		id:'btnplay_'+ids,
+		volume: volume,
+		url: mp3url
+	  });
 
-      if(flg == 'play'){
-        <?php 
-        if(get_option('capmc_audio_disable_simultaneous_play') == '1'){
-        	echo 'stop_all_tracks();';
-        }
-        ?>
+	  if(flg == 'play'){
+		<?php 
+		if(get_option('capmc_audio_disable_simultaneous_play') == '1'){
+			echo 'stop_all_tracks();';
+		}
+		?>
 		soundManager.play('btnplay_'+ids,{
 			onfinish: function() {
 				if(loops == 'true'){
@@ -56,46 +56,100 @@ function capmc_footer_code(){
 				}
 			}
 		});
-      }
-      else if(flg == 'stop'){
-        //soundManager.stop('btnplay_'+ids);
-    	soundManager.pause('btnplay_'+ids);
-      }
-    }
-    function show_hide(flag,ids)
-    {
-      if(flag=='play'){
-        document.getElementById('btnplay_'+ids).style.display = 'none';
-        document.getElementById('btnstop_'+ids).style.display = 'inline';
-      }
-      else if (flag == 'stop'){
-        document.getElementById('btnplay_'+ids).style.display = 'inline';
-        document.getElementById('btnstop_'+ids).style.display = 'none';
-      }
-    }
-    function loopSound(soundID) 
-    {
-    	window.setTimeout(function() {
-    		soundManager.play(soundID,{onfinish:function(){loopSound(soundID);}});
-    	},1);
-    }
-    function stop_all_tracks()
-    {
-    	soundManager.stopAll();
+	  }
+	  else if(flg == 'stop'){
+		//soundManager.stop('btnplay_'+ids);
+		soundManager.pause('btnplay_'+ids);
+	  }
+	}
+	function show_hide(flag,ids)
+	{
+	  if(flag=='play'){
+		document.getElementById('btnplay_'+ids).style.display = 'none';
+		document.getElementById('btnstop_'+ids).style.display = 'inline';
+	  }
+	  else if (flag == 'stop'){
+		document.getElementById('btnplay_'+ids).style.display = 'inline';
+		document.getElementById('btnstop_'+ids).style.display = 'none';
+	  }
+	}
+	function loopSound(soundID) 
+	{
+		window.setTimeout(function() {
+			soundManager.play(soundID,{onfinish:function(){loopSound(soundID);}});
+		},1);
+	}
+	function stop_all_tracks()
+	{
+		soundManager.stopAll();
 		var inputs = document.getElementsByTagName("input");
-    	for (var i = 0; i < inputs.length; i++) {
-    		if(inputs[i].id.indexOf("btnplay_") == 0){
-    			inputs[i].style.display = 'inline';//Toggle the play button
-        	}
-    		if(inputs[i].id.indexOf("btnstop_") == 0){
-    	    	inputs[i].style.display = 'none';//Hide the stop button
-    		}
-    	}        
-    }
+		for (var i = 0; i < inputs.length; i++) {
+			if(inputs[i].id.indexOf("btnplay_") == 0){
+				inputs[i].style.display = 'inline';//Toggle the play button
+			}
+			if(inputs[i].id.indexOf("btnstop_") == 0){
+				inputs[i].style.display = 'none';//Hide the stop button
+			}
+		}
+	}
 	</script>
 	<?php
 }
 add_action('wp_footer', 'capmc_footer_code');
+
+function capmc_embed_player_settings_handler($atts, $content = null) 
+{
+	extract(shortcode_atts(array(
+		'volume' => '',
+		'class' => '',
+	), $atts, 'capmc_embed_player_settings'));	
+
+	if(empty($volume)){
+		// Let the lower-level function determine the default.
+		$volume_attr = "";
+	} else {
+		$volume = (int) $volume;
+		if ( 0 > $volume || 100 < $volume ) {
+			// Let the lower-level function determine the default.
+			$volume_attr = "";
+		}
+		$volume_attr = 'volume="'. $volume . '"';
+	}
+	
+	if(empty($class)){
+		// Let the lower-level function determine the default.
+		$class_attr = "";
+	} else {
+		$class_attr = 'class="'. $class . '"';
+	}
+	
+	$fileurl = get_option('capmc_file_url');
+	if(empty($fileurl)){
+		return '<div style="color:red;font-weight:bold;">Compact Audio Player Error! You must enter the mp3 file URL via the "File URL" parameter in the Dashboard Settings. Please check the documentation for more info.</div>';
+	}
+	
+	// Determine autoplay value. Defaults to autoplay.
+	$autoplay_attr = "autoplay=true"; // Default value
+	if( get_option('capmc_autoplay') !== FALSE ) {
+		// Option is set
+		if( !get_option('capmc_autoplay') ) {
+			// Option is set but unchecked. Do not autoplay.
+			// From experience, cannot depend on autoplay=false to stop autoplay.
+			// To stop autoplay, we have to completely leave out the
+			// shortcode attribute (argument) autoplay.
+			$autoplay_attr = "";
+		} // else Use default value.
+	} // else Option is not set. Use default value.
+	
+	echo do_shortcode('[capmc_embed_player '
+			. $autoplay_attr .' '
+			. $class_attr .' '
+			. $volume_attr
+			. ' loops="'. get_option('capmc_loop', "true")
+			. '" fileurl="'. $fileurl
+			. '"]'
+		);
+}
 
 function capmc_embed_player_handler($atts, $content = null) 
 {
@@ -117,8 +171,8 @@ function capmc_embed_player_handler($atts, $content = null)
 	$player_cont = '<div class="'.$class.'">';
 	$player_cont .= '<input type="button" id="btnplay_'.$ids.'" class="myButton_play" onClick="play_mp3(\'play\',\''.$ids.'\',\''.$fileurl.'\',\''.$volume.'\',\''.$loops.'\');show_hide(\'play\',\''.$ids.'\');" />';
 	$player_cont .= '<input type="button"  id="btnstop_'.$ids.'" style="display:none" class="myButton_stop" onClick="play_mp3(\'stop\',\''.$ids.'\',\'\',\''.$volume.'\',\''.$loops.'\');show_hide(\'stop\',\''.$ids.'\');" />';
- 	$player_cont .= '<div id="sm2-container"><!-- flash movie ends up here --></div>';
- 	$player_cont .= '</div>';
+	$player_cont .= '<div id="sm2-container"><!-- flash player ends up here --></div>';
+	$player_cont .= '</div>';
 
 	if(!empty($autoplay)){
 $path_to_swf = C_AUDIO_MORE_CTL_BASE_URL .'swf/soundmanager2.swf';
@@ -134,7 +188,7 @@ soundManager.setup({
 		});
 		var auto_loop = '$loops';
 		mySound.play({
-    		onfinish: function() {
+			onfinish: function() {
 				if(auto_loop == 'true'){
 					loopSound('btnplay_$ids');
 				}
@@ -142,10 +196,10 @@ soundManager.setup({
 					document.getElementById('btnplay_$ids').style.display = 'inline';
 					document.getElementById('btnplay_$ids').style.display = 'none';
 				}
-    		}
+			}
 		});
 		document.getElementById('btnplay_$ids').style.display = 'none';
-        document.getElementById('btnstop_$ids').style.display = 'inline';
+		document.getElementById('btnstop_$ids').style.display = 'inline';
 	},
 	ontimeout: function() {
 		// SM2 could not start. Missing SWF? Flash blocked? Show an error.
@@ -154,11 +208,12 @@ soundManager.setup({
 });
 </script>
 EOT;
-}//End autopay code
+	}//End autopay code
 
 	return $player_cont;
 }
 add_shortcode('capmc_embed_player', 'capmc_embed_player_handler');
+add_shortcode('capmc_embed_player_settings', 'capmc_embed_player_settings_handler');
 if (!is_admin()){add_filter('widget_text', 'do_shortcode');}
 add_filter('the_excerpt', 'do_shortcode',11);
 
@@ -169,64 +224,76 @@ function capmc_mp3_player_admin_menu() {
 }
 
 function capmc_mp3_options() 
-{	
+{
 	echo '<div class="wrap">';
 	echo '<div id="poststuff"><div id="post-body">';
 	echo '<div id="icon-upload" class="icon32"><br></div><h2>Compact Audio Player More Control</h2>';
 	
 	echo '<div style="background: #FFF6D5; border: 1px solid #D1B655; color: #3F2502; padding: 15px 10px">Visit the <a href="http://www.techborder.com/wordpress-plugin-compact-audio-player-more-control" target="_blank">Compact Audio Player More Control</a> plugin page for more documentation.</div>';
-	echo "<p>This is a simple all browser supported audio player based on Compact Audio Player. There are extra settings if you want to use them. You can just add the shortcode with the MP3 file URL in a WordPress post or page to embed the audio player.</p>";
-	echo "<h3>Shortcode Format</h3>";
+	echo "<p>This is a simple all browser supported audio player based on Compact Audio Player. There are extra settings if you want to use them. You can just add the shortcode in a WordPress theme, post or page to embed the audio player.</p>";
+	echo '<h3>Shortcode Format</h3>';
+	echo '<h4>Controlled via Settings</h4>';
+	echo "<p><code>[capmc_embed_player_settings]</code></p>";
+	echo "<p>For more on how to use the shortcode, go to the website above to learn how to add it to your theme and control the audio player with the settings below.</p>";
+	echo "<h4>Legacy</h4>";
+	echo "<p>Using the legacy shortcode does not use most of the settings below. The only setting that will have an affect with the legacy shortcode is 'Disable Simultaneous Play'</p>";
 	echo '<p><code>[capmc_embed_player fileurl="URL OF THE MP3 FILE"]</code></p>';	
 	echo '<p><strong>Example:</strong></p>';
 	echo '<p><code>[capmc_embed_player fileurl="http://www.example.com/wp-content/uploads/my-music/mysong.mp3"]</code></p>';
-	echo "<p>For even more control, go to the website above to learn how to add it to your theme and control the audio player with the settings below.</p>";
 
-		
 	if(isset($_POST['capmc_player_settings'])){
 		update_option('capmc_audio_disable_simultaneous_play', isset($_POST["capmc_audio_disable_simultaneous_play"])?'1':'');
-		update_option('capmc_audio_file', isset($_POST["capmc_audio_file"])?$_POST["capmc_audio_file"]:'');
+		update_option('capmc_file_url', isset($_POST["capmc_file_url"])?$_POST["capmc_file_url"]:'');
 		update_option('capmc_autoplay', isset($_POST["capmc_autoplay"])?'1':'');
+		update_option('capmc_loop', isset($_POST["capmc_loop"])?'1':'');
 	}
 		
 	?>
-    <form method="post" action="<?php echo $_SERVER["REQUEST_URI"]; ?>">
+	<form method="post" action="<?php echo $_SERVER["REQUEST_URI"]; ?>">
 
 	<div class="postbox">
 	<h3><label for="title">Compact Audio Player More Control Settings</label></h3>
 	<div class="inside">
 	
-    <table class="form-table">
-    
-    <tr valign="top"><td width="25%" align="left">
-    Disable Simultaneous Play: 
-    </td><td align="left">    
-    <input name="capmc_audio_disable_simultaneous_play" type="checkbox"<?php if(get_option('capmc_audio_disable_simultaneous_play')!='') echo ' checked="checked"'; ?> value="1"/>
-    <br /><p class="description">Check this option if you only want to allow one audio file to be played at a time (helpful if you have multiple audio files on a page). It will automatically stop the audio file that is currently playing when a user plays a new file.</p>
-    </td></tr>
+	<table class="form-table">
 	
-    <tr valign="top"><td width="25%" align="left">
-    Audio File: 
-    </td><td align="left">    
-    <input name="capmc_audio_file" type="text" <?php if(get_option('capmc_audio_file')!='') echo ' value="'. get_option('capmc_audio_file') . '"'; ?> />
-    <br /><p class="description">File to be played.</p>
-    </td></tr>
+	<tr valign="top"><td width="25%" align="left">
+	Disable Simultaneous Play: 
+	</td><td align="left">    
+	<input name="capmc_audio_disable_simultaneous_play" type="checkbox"<?php if(get_option('capmc_audio_disable_simultaneous_play')!='') echo ' checked="checked"'; ?> value="1"/>
+	<br /><p class="description">Check this option if you only want to allow one audio file to be played at a time (helpful if you have multiple audio files on a page). It will automatically stop the audio file that is currently playing when a user plays a new file.</p>
+	</td></tr>
+	
+	<tr valign="top"><td width="25%" align="left"><label for="capmc_file_url">
+	File URL:
+	</label>
+	</td><td align="left">    
+	<input name="capmc_file_url" type="text" maxlength="100" size="50" <?php if(get_option('capmc_file_url')!='') echo ' value="'. get_option('capmc_file_url') . '"'; ?> />
+	<br /><p class="description">File to be played.</p>
+	</td></tr>
 
-    <tr valign="top"><td width="25%" align="left">
-    Autoplay: 
-    </td><td align="left">    
-    <input name="capmc_autoplay" type="checkbox"<?php if(get_option('capmc_autoplay')!='') echo ' checked="checked"'; ?> value="1"/>
-    <br /><p class="description">Automatically play the audio when a visitor comes to your site. Does not work for iOS devices (Apple doesn't allow it).</p>
-    </td></tr>
-    
-    </table>	
+	<tr valign="top"><td width="25%" align="left"><label for="capmc_autoplay">
+	Auto-play: 
+	</label></td><td align="left">    
+	<input name="capmc_autoplay" type="checkbox"<?php if(get_option('capmc_autoplay')!='') echo ' checked="checked"'; ?> value="1"/>
+	<br /><p class="description">Automatically play the audio when a visitor comes to your site. Does not work for iOS devices (Apple doesn't allow it).</p>
+	</td></tr>
 	
-    <div class="submit">
-   	<input type="submit" class="button-primary" name="capmc_player_settings" value="<?php _e('Update'); ?>" />
-    </div>
-    
-    
-    </div></div>
+	<tr valign="top"><td width="25%" align="left"><label for="capmc_loop">
+	Loop: 
+	</label></td><td align="left">    
+	<input name="capmc_loop" id="capmc_loop" type="checkbox"<?php if(get_option('capmc_loop')!='') echo ' checked="checked"'; ?> value="1"/>
+	<br /><p class="description">If checked, then repeat audio file.</p>
+	</td></tr>
+	
+	</table>	
+	
+	<div class="submit">
+	<input type="submit" class="button-primary" name="capmc_player_settings" value="<?php _e('Update'); ?>" />
+	</div>
+	
+	
+	</div></div>
 	</form>
 
 	<?php
